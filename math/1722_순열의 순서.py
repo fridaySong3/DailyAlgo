1c56c5aef58ca5e994202579dc5b41e05fc971cb
fridaySong3 <pleasure5567@gmail.com>
2022-09-30 00:47:33 +0900

    Fix boj 1722

--- math/1722_순열의 순서.py
+++ math/1722_순열의 순서.py
@@ -1,30 +1,34 @@
 # https://www.acmicpc.net/problem/1722
+from operator import mul
 import sys
-
-def next_perm(perm):
-    for i in range(N-1, 0, -1):
-        if perm[i-1] < perm[i]:
-            for j in range(N-1, i-1, -1):
-                if perm[i-1] < perm[j]:
-                    perm[i-1], perm[j] = perm[j], perm[i-1]
-                    perm[i:] = list(reversed(perm[i:]))
-                    break
-            break
+from itertools import accumulate
 
 N = int(sys.stdin.readline())
 code, *k_or_p = map(int, sys.stdin.readline().split())
-perm = list(range(1, N+1))
+
+fac = list(reversed(list(accumulate(range(1, N), mul))))
+cnt_small = [0] * (N-1)
 if code == 1:
     k = k_or_p[0]
-    for i in range(k-1):
-        next_perm(perm)
-    print(' '.join(map(str, perm)))
-else:
-    p = k_or_p
-    cnt_k = 1
-    while True:
-        if perm == p:
-            print(cnt_k)
+    q = k - 1
+    for i in range(N-1):
+        q, r = divmod(q, fac[i])
+        cnt_small[i] = q
+        if r == 0:
             break
-        next_perm(perm)
-        cnt_k += 1
+        q = r
+    perm = []
+    sorted_p = list(range(1, N+1))
+    for i in range(N-1):
+        perm.append(sorted_p[cnt_small[i]])
+        del sorted_p[cnt_small[i]]
+    perm.append(sorted_p[0])
+    print(' '.join(map(str, perm)))
+else:  
+    perm = k_or_p
+    for i in range(N-1):
+        for j in range(i+1, N):
+            if perm[i] > perm[j]:
+                cnt_small[i] += 1
+    k = sum([cnt * f for cnt, f in zip(cnt_small, fac)]) + 1
+    print(k)
