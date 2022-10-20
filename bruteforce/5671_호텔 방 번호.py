9b32a729d4b3ae9cc1d34cbe08128e8a1bb30aa2
fridaySong3 <pleasure5567@gmail.com>
2022-10-20 23:49:14 +0900

    Again boj 5671

--- bruteforce/5671_호텔 방 번호.py
+++ bruteforce/5671_호텔 방 번호.py
@@ -1,14 +1,20 @@
 # https://www.acmicpc.net/problem/5671
 import sys
+
+M = 5000
+dp = [0] * (M+1)
+
+for n in range(1, M+1):
+    sn = str(n)
+    if len(sn) == len(set(sn)):
+        dp[n] = dp[n-1] + 1
+    else:
+        dp[n] = dp[n-1]
+
 while True:
     try:
         N, M = map(int, sys.stdin.readline().split())
     except:
         break
     else:
-        ans = 0
-        for i in range(N, M+1):
-            si = str(i)
-            if len(si) == len(set(si)):
-                ans += 1
-        print(ans)
+        print(dp[M] - dp[N-1])
